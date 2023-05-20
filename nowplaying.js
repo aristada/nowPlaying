67359392064602a438495d9e8fde6aee8bbad092
Ari <45417388+NotSociallyFit@users.noreply.github.com>
2023-05-19 21:14:30 -0400

    now with 100% more threading

--- nowplaying.js
+++ nowplaying.js
@@ -1,31 +1,26 @@
 // get your own last.fm api key from https://www.last.fm/api/account/create
-LASTFM_API_KEY = "YOUR_KEY_HERE"
+LASTFM_API_KEY = "YOUR_API_KEY"
 username = "YOUR_USERNAME_HERE" // change username here
 STRING_ID = "music" // change string id here
 url = "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&format=json&api_key=" + LASTFM_API_KEY + "&limit=1&user=" + username
 
 // make API call
-function httpGet(url) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", url, false); // false for synchronous request
-    xmlHttp.send(null);
-    return xmlHttp.responseText;
+const fetchURL = async (url) => {
+    var response = await fetch(url);
+    return response.json();
 }
 
+const updateNowPlaying = async () => {
+    var json = await fetchURL(url);
+    var last_track = json.recenttracks.track[0];
+    var track = last_track.name;
+    var trackLink = last_track.url;
+    var artist = last_track.artist['#text'];
 
+    var now_playing = (last_track["@attr"] == undefined) ? false : true;
 
-var json = JSON.parse(httpGet(url))
-var last_track = json.recenttracks.track[0]
-var track = last_track.name
-var trackLink = last_track.url
-var artist = last_track.artist['#text']
+    var html = document.getElementById(STRING_ID);
+    html.innerHTML = "🎵: " + track + " by " + artist + " - " + (now_playing ? " Now Playing" : " Last Played");
+}
 
-var now_playing = (last_track["@attr"] == undefined) ? false : true
-
-var html = document.getElementById(STRING_ID);
-html.innerHTML = "𝅘𝅥𝅮: " + last_track.name + " by " + last_track.artist['#text'] + " - " +  (now_playing ? " Now Playing" : " Last Played");
-
-console.log(
-    "Artist: " + artist + "\n" +
-    "Track: " + track + "\n" +
-    "Now playing: " + now_playing)
+updateNowPlaying();
